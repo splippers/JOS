432eb6435c599310ab639a4ed906bff9d83d7fe4
Jonathan Davis <132134464+splippers@users.noreply.github.com>
2026-04-26 17:11:58 +0100

    Create build/build-initrd.sh

---
+++ build/build-initrd.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Build JOS initramfs
+
+set -e
+
+echo "Building JOS initramfs..."
+
+mkdir -p build/out
+
+cd initrd
+find . | cpio -H newc -o > ../build/out/initrd.img
+cd ..
+
+echo "Initramfs built: build/out/initrd.img"
